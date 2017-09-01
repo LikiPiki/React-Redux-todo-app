a7c80e4c664f3202a17f654662559d00571094e8
Илья <ilja.batalev@gmail.com>
2017-09-01 21:44:23 +0300

    skelet for main.go

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -18,7 +23,7 @@ func init() {
 	var err error
 	db, err = gorm.Open("sqlite3", "test.db")
 	if err != nil {
-		panic("failed to connect database", err)
+		panic("failed to connect database")
 	}
 	defer db.Close()
 
@@ -28,4 +33,60 @@ func init() {
 }
 
 func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", handleFuncMain).Path("GET")
+	router.HandleFunc("/all_todos", getAllTodos).Path("GET")
+	router.HandleFunc("/show_todo", showTodo).Path("GET")
+	router.HandleFunc("/add_todo", addTodo).Path("POST")
+	router.HandleFunc("/delete_todo", deleteTodo).Path("POST")
+	router.HandleFunc("is_checked_todo", complitedTodo).Path("POST")
+	router.HandleFunc("edit_todo", editTodo).Path("POST")
+}
+
+func handleFuncMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w /*Main*/)
+}
+
+func getAllTodos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w /*JSON witch have all todos*/)
+}
+
+func showTodo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w /*JSON witch have only one todo*/)
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	// Add todo to db
+	if err != nil { // Error which can appeared when todo is added
+		fmt.Fprintln(w, "error")
+	} else {
+		fmt.Fprintln(w, "ok")
+	}
+}
+
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	// Delete todo from db
+	if err != nil { // Error which can appeared when todo is deleted
+		fmt.Fprintln(w, "error")
+	} else {
+		fmt.Fprintln(w, "ok")
+	}
+}
+
+func complitedTodo(w http.ResponseWriter, r *http.Request) {
+	// Checked todo
+	if err != nil { // Error which can appeared when todo is complited
+		fmt.Fprintln(w, "error")
+	} else {
+		fmt.Fprintln(w, "ok")
+	}
+}
+
+func editTodo(w http.ResponseWriter, r *http.Request) {
+	// Edit todo to db
+	if err != nil { // Error which can appeared when todo is edited
+		fmt.Fprintln(w, "error")
+	} else {
+		fmt.Fprintln(w, "ok")
+	}
 }
